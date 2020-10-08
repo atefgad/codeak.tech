0d88a1696b01d7e7eddb3c9aff73d4941f5105e8
atefgad <atef0777@gmail.com>
2020-10-08 18:15:34 +0200

    updated

--- js/main.js
+++ js/main.js
@@ -8,9 +8,45 @@
 $(document).ready(function () {
     "use strict";
 
-    
+    /*======================================
+     WOW Animation
+     ======================================*/
+     var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 0, // distance to the element when triggering the animation (default is 0)
+        mobile: false, // trigger animations on mobile devices (default is true)
+        live: true, // act on asynchronously loaded content (default is true)
+        callback: function (box) {
+        }
+        , scrollContainer: true // optional scroll container selector, otherwise use window
+    }
+    );
+    wow.init();
 
-    new WOW().init();
+    /*======================================
+     OwlCarousel our works
+     ======================================*/
+    $('.list_our_works').owlCarousel({
+        loop: true,
+        items:3,
+        margin: 10,
+        responsiveClass: true,
+        nav: false,
+        dots: true,
+        responsive:{
+        600:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:3,
+            nav:false,
+        }
+        }
+    })
+
+
 });
 
 $(window).scroll(function() {
